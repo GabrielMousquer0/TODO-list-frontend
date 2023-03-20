a6cce3684a0a97d44c4f004e45d01f2409fd4103
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-03-20 08:14:05 -0300

    ✨ Adicionando botão para excluir elementos da lista a partir do seu ID

---
+++ list-front/src/components/list/events/deleteElement.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { useMutation, useQuery  } from 'villus'
+
+const props = defineProps({
+        itemId: {
+          type: String
+        }
+      })
+
+const fetchQuery = `
+query fetchQuery {
+    lists {
+        title
+        content
+        id
+    }
+}
+`
+const deleteElement = `
+    mutation deleteElement($id: ID!) {
+        deleteElement(id: $id) {
+            title
+            content
+            id
+    }
+}
+`
+
+const { data } = useQuery({
+  query: fetchQuery
+})
+const { execute } = useMutation(deleteElement)
+function deleteElements(id) {
+execute({
+    id: id
+})
+}
+</script>
+<template>
+    <div>
+        <q-btn  square icon="delete" color="primary" style="left: 700px; bottom: 70px;" @click="deleteElements(props.itemId)"/>
+    </div>
+             
+
+
+</template>
